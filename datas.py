6ba9ef5c4f1f1fd004a94ac18fe1bc04768278bd
Matheus Nasicmento <matheus.bellio.nascimento@gmail.com>
2020-03-22 18:59:10 -0300

    Exercício class Data

--- datas.py
+++ datas.py
@@ -1,4 +1,17 @@
 # EXERCICIO FORMATAR DATA COM CLASSE.METODO
+'''
+Lançamos o seguinte desafio: para ajudar na formatação de datas você deve criar uma nova classe auxiliar.
+Essa classe deve representar uma Data (sem hora) que sabe imprimir uma data formatada. Ela deve funcionar dessa forma:
+
+from datas import Data
+d = Data(21,11,2007)
+d.formatada()
+Imprime:
+21/11/2007
+
+Crie e implemente essa classe dentro de uma arquivo datas.py. Mãos à obra!
+'''
+
 class Data:
     def __init__(self, dia, mes, ano):
         self.dia = dia
